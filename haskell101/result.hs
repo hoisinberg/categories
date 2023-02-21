74fcf57035aa208db27e39a3dd9ebdab5e3ad3e3
Michael <michaelq94@gmail.com>
2023-02-21 00:38:58 -0800

    Result type capturing either a value or a stack of errors

---
+++ haskell101/result.hs
@@ -0,0 +1,38 @@
+module Result where
+import qualified GHC.Err as GHC
+import Distribution.Simple (VersionInterval)
+
+type Result v = Either v [String]
+
+value :: v -> Result v
+value = Left
+
+errors :: [String] -> Result v
+errors = Right
+
+error :: String -> Result v
+error s = errors [s]
+
+get :: Result v -> v
+get (Left v) = v
+get (Right e) = GHC.error (show e)
+
+orDefault :: Result v -> v -> v
+orDefault (Left v) _ = v
+orDefault (Right e) d = d
+
+fmap :: (v -> Result w) -> Result v -> Result w
+fmap f (Left v) = f v
+fmap _ (Right e) = Right e
+
+map :: (v -> w) -> Result v -> Result w
+map f (Left v) = (value . f) v
+map _ (Right e) = Right e
+
+orTry :: Result v -> Result v -> Result v
+orTry (Left v) _ = Left v
+orTry (Right e) x = x
+
+onFailure :: Result v -> String -> Result v
+onFailure (Left v) _ = Left v
+onFailure (Right e) s = Right (s:e)
