e49a545bfb6d044b368ec2e1bc530ce5bd57485a
Michael <michaelq94@gmail.com>
2023-03-24 22:52:32 -0700

    Re-implementation of Ex11 using existing functions

--- haskell101/haskell99/Ex11.hs
+++ haskell101/haskell99/Ex11.hs
@@ -1,17 +1,12 @@
 module Ex11 (encodeModified) where
-import Lib.Either
+import Ex10
+import Lib.ListOp
 
 data Encoded a = Multiple Int a | Single a deriving (Show)
 
 encodeModified :: (Eq a) => [a] -> [Encoded a]
-encodeModified [] = []
-encodeModified (x:xs) = accumulatingEncodeModified (Single x) xs 
+encodeModified = fmap' makeDesc . encode
 
-accumulatingEncodeModified :: (Eq a) => Encoded a -> [a] -> [Encoded a]
-accumulatingEncodeModified e [] = [e]
-accumulatingEncodeModified (Single x) (y:ys)
-  | x == y = accumulatingEncodeModified (Multiple 2 x) ys
-  | otherwise = Single x : accumulatingEncodeModified (Single y) ys
-accumulatingEncodeModified (Multiple n x) (y:ys)
-  | x == y = accumulatingEncodeModified (Multiple (n+1) x) ys
-  | otherwise = Multiple n x : accumulatingEncodeModified (Single y) ys
+makeDesc :: (Int, a) -> Encoded a
+makeDesc (1, x) = Single x
+makeDesc (n, x) = Multiple n x
