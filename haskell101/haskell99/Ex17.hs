17a87600b0eca137a4925df93c2910016bbf640b
Michael <michaelq94@gmail.com>
2023-03-25 00:10:42 -0700

    Improved Ex17

--- haskell101/haskell99/Ex17.hs
+++ haskell101/haskell99/Ex17.hs
@@ -1,12 +1,9 @@
-module Ex17(split) where
+module Ex17 (split) where
+
 import Lib.ListOp
 
 split :: Int -> [a] -> ([a], [a])
-split n l
-  | n >= 0 = accumulatingSplit n [] l
-  | otherwise = ([], l)
-
-accumulatingSplit :: Int -> [a] -> [a] -> ([a], [a])
-accumulatingSplit 0 l1 l2 = (l1, l2)
-accumulatingSplit _ l [] = (l, [])
-accumulatingSplit n l (x:xs) = accumulatingSplit (n-1) (append' x l) xs
+split n l =
+  ( (fmap' snd . filter' (\(i, _) -> i < n) . index') l,
+    (fmap' snd . filter' (\(i, _) -> i >= n) . index') l
+  )
